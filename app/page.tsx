fd9568826b8aa336a827a2bb382154b09795dcee
jormarcus <jormarcus92@gmail.com>
2023-04-24 20:10:12 -0400

    Setup upstash redis db

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
+import { db } from '@/lib/db';
+
+export default async function Home() {
+  await db.set('hello', 'baike');
+
   return <div className="text-rose-500">Hello Baike</div>;
 }
