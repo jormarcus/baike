ff0510cc56577be811534637eb0e96b31563490a
jormarcus <jormarcus92@gmail.com>
2023-04-29 23:30:03 -0400

    Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Nunito } from 'next/font/google';
 import './globals.css';
 import ClientOnly from './components/ui/ClientOnly';
 import LoginModal from './components/modals/LoginModal';
+import Navbar from './components/navbar/Navbar';
+import RegisterModal from './components/modals/RegisterModal';
 
 export const metadata = {
   title: 'Baike',
@@ -23,6 +25,8 @@ export default function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <LoginModal />
+          <RegisterModal />
+          <Navbar />
         </ClientOnly>
         <div className="pb-20 pt-28">{children}</div>
       </body>
