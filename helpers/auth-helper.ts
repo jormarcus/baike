2c81e918f2c6e8a12621236702c89aef6262dfd9
jormarcus <jormarcus92@gmail.com>
2023-04-29 23:20:03 -0400

    Fix provider logins

--- helpers/auth-helper.ts
+++ helpers/auth-helper.ts
@@ -1,11 +1,27 @@
-export function getCredentials(isGoogle: boolean) {
-  const key = isGoogle ? 'GOOGLE_CLIENT' : 'GITHUB';
-  const clientId = process.env[`${key}]_ID`];
-  const clientSecret = process.env[`${key}]_SECRET`];
+export const CredentialProviders = {
+  GOOGLE: 'GOOGLE_CLIENT',
+  GITHUB: 'GITHUB',
+} as const;
+
+type ObjectValues<T> = T[keyof T];
+
+type CredentialProvider = ObjectValues<typeof CredentialProviders>;
+
+export function getCredentials(credentialProvider: CredentialProvider) {
+  let clientId: string | undefined;
+  let clientSecret: string | undefined;
+
+  if (credentialProvider === CredentialProviders.GOOGLE) {
+    clientId = process.env.GOOGLE_CLIENT_ID;
+    clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+  } else if (credentialProvider === CredentialProviders.GITHUB) {
+    clientId = process.env.GITHUB_ID;
+    clientSecret = process.env.GITHUB_SECRET;
+  }
 
   if (!clientId || !clientSecret) {
     throw new Error(
-      `Please provide ${key}_ID and  ${key}_SECRET env variables`
+      `Please provide ${credentialProvider}_ID and  ${credentialProvider}_SECRET env variables`
     );
   }
 
