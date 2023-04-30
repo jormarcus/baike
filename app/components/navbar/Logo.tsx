dfabba7849e75870298a613174b55cb6ad93ded6
jormarcus <jormarcus92@gmail.com>
2023-04-29 23:30:03 -0400

    Navbar

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <h2
+      onClick={() => router.push('/')}
+      className="text-lg text-rose-500 font-bold"
+    >
+      BAIKE
+    </h2>
+  );
+};
+
+export default Logo;
