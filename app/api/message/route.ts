bf651b0392e24884edda4606f7420f120252e1e7
jormarcus <jormarcus92@gmail.com>
2023-05-20 11:04:48 -0400

    Message route and openai stream

--- app/api/message/route.ts
+++ app/api/message/route.ts
@@ -1,3 +1,43 @@
+import { MessageArraySchema } from '@/lib/validators/message';
+import {
+  ChatGPTMessage,
+  OpenAIStream,
+  OpenAIStreamPayload,
+} from '@/lib/openai-stream';
+import { recipePrompt } from '@/helpers/prompts/recipePrompt';
+
 export async function POST(req: Request) {
-  console.log('posting');
+  const request = await req.json();
+  console.log('request', request);
+  const messages = request.message;
+
+  const parsedMessages = MessageArraySchema.parse([messages]);
+
+  const outboundMessages: ChatGPTMessage[] = parsedMessages.map((message) => {
+    return {
+      role: message.isUserMessage ? 'user' : 'system',
+      content: message.text,
+    };
+  });
+
+  outboundMessages.unshift({
+    role: 'system',
+    content: recipePrompt,
+  });
+
+  const payload: OpenAIStreamPayload = {
+    model: 'gpt-3.5-turbo',
+    messages: outboundMessages,
+    temperature: 0.4,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 150,
+    stream: true,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+
+  return new Response(stream);
 }
---
+++ helpers/prompts/recipePrompt.ts
@@ -0,0 +1,15 @@
+export const recipePrompt = `
+You are a chatbot on a website that helps people find recipes. 
+Users can ask you to find specific recipes for them.
+Users can ask you to find recipes for them based on ingredients they provide.
+Users can ask you for recipes based on a specific diet.
+Users can ask you for recipes based on a specific cuisine.
+
+Try to provide four links to online recipes.
+When providing recipes, put the ingredients on separate lines with bullet points at the start and then a space and the quantities and units.
+
+If a user asks you to compare recipes, provide a list of the ingredients for each recipe with bullet points at the start and then a space and the quantities and units. Highlight the differences between the recipes.
+`;
+
+// You are a helpful assistant who can create a table with ingredients and their quantities. Please create a table for the following two lists:
+// Please create a table with separate columns for the quantities based on the respective lists. Each row should include the ingredient, the quantity from List 1, and the quantity from List 2. Thank you!
