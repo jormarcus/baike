c63301b807f0f2e11ecd6b7b67e8716c24ab440a
jormarcus <jormarcus92@gmail.com>
2023-06-27 21:39:18 -0400

    Use markdown component to show links in a msg

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,21 +1,27 @@
-import { Message as Messagetype } from '@/lib/validators/message';
+'use client';
+
+import { SafeMessage } from '@/types';
+import MarkdownLite from './MarkdownLite';
 interface MessageProps {
-  message: Messagetype;
+  message: SafeMessage;
 }
 
 const Message: React.FC<MessageProps> = ({ message }) => {
+  console.log('message', message);
   return (
     <div
-      className={`flex items-center justify-between w-full px-4 py-2
+      className={`flex items-center justify-between w-full px-4 py-2 dark:text-neutral-200
     ${
       message.isUserMessage
-        ? 'flex-row-reverse dark:bg-gray-600'
-        : 'flex-row dark:bg-gray-800'
+        ? 'flex-row-reverse dark:bg-neutral-600'
+        : 'flex-row dark:bg-neutral-800'
     }
     `}
     >
       <div>
-        <p>{message.text}</p>
+        <p>
+          <MarkdownLite text={message.text} />
+        </p>
       </div>
     </div>
   );
