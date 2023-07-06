adb565dd2bbaa80725a756eb0a5fabaadb959a42
jormarcus <jormarcus92@gmail.com>
2023-07-06 12:08:17 -0400

    Post comments

---
+++ src/app/_actions/post-actions.ts
@@ -0,0 +1,11 @@
+'use server';
+
+export async function addComment(comment: string, postId: string) {
+  console.log('adding comment', comment);
+  // const newComment = await prisma.comment.create({
+  //   data: {
+  //     comment,
+  //     postId,
+  //   },
+  // });
+}
---
+++ src/components/feed/PostComments.tsx
@@ -0,0 +1,68 @@
+import { useMemo, useState } from 'react';
+
+import { Input } from '../inputs/Input';
+import { Button } from '../ui/Button';
+import { addComment } from '@/app/_actions/post-actions';
+import { Icons } from '../Icons';
+
+const PostComments: React.FC<{ postId: string; commentsCount: number }> = ({
+  postId,
+  commentsCount,
+}) => {
+  const [input, setInput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const isEmpty = useMemo(() => input.length === 0, [input]);
+
+  const handleInputChange = (e: any) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      await addComment(e.target.value, postId);
+      setInput('');
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="mt-2">
+      {commentsCount === 0 ? null : (
+        <div className="text-sm text-neutral-400 hover:text-neutral-300 cursor-pointer">{`View all ${commentsCount} comments`}</div>
+      )}
+      <div>
+        <form onSubmit={handleSubmit}>
+          <Input
+            id="add-comment"
+            className="w-full max-w-[470px] mt-1 border-none p-0 focus:ring-0 focus-visible:ring-0 text-neutral-400"
+            value={input}
+            placeholder="Add a comment..."
+            onChange={handleInputChange}
+            disabled={isLoading}
+          />
+          <Button
+            type="submit"
+            onClick={(e) => handleSubmit(e)}
+            className={`absolute bottom-0 right-0 text-neutral-500 bg-transparent hover:bg-transparent m-2 h-8 px-2
+          ${
+            isEmpty
+              ? 'cursor-default'
+              : 'bg-amber-500 text-neutral-200 hover:bg-amber-500'
+          }
+          `}
+          >
+            <Icons.arrowRightCircle className="h-5 w-5" />
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default PostComments;
