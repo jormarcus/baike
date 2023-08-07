6edc2e494bf18c2401407a8250c822ed39626f65
jormarcus <jormarcus92@gmail.com>
2023-08-07 11:50:02 -0400

    Remove categories from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Feed from '@/components/feed/Feed';
-import Categories from '@/components/categories/Categories';
 import Logo from '@/components/ui/Logo';
 import ChatInputHome from '@/components/chat/ChatInputHome';
 import { getFeedPosts } from './_actions/post-actions';
@@ -10,7 +9,6 @@ export default async function Home() {
     <div className="flex items-center flex-col justify-center">
       <Logo className="pb-6 md:pb-6 py-6 cursor-default" />
       <ChatInputHome />
-      <Categories />
       <Feed initialPosts={posts} />
     </div>
   );
