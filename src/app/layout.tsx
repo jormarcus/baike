f59b117dccb0dba713095749657eb8ba4473b10b
jormarcus <jormarcus92@gmail.com>
2023-08-30 22:35:23 -0400

    SupressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
 }) {
   const currentUser = await getCurrentUser();
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body>
         <Providers>
           <div className={`${font.className}`}>
