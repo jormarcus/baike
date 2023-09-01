5779467900fcc3f49e4d202a9fa350e564d8e982
jormarcus <jormarcus92@gmail.com>
2023-08-31 23:12:36 -0400

    Ingredient List framer motion animations

--- src/components/recipes/IngredientsList.tsx
+++ src/components/recipes/IngredientsList.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import { useState } from 'react';
+import { CheckedState } from '@radix-ui/react-checkbox';
+import { motion, stagger, useAnimate } from 'framer-motion';
+
 import { SafeIngredient } from '@/types';
 import { Checkbox } from '../ui/Checkbox';
 import { Label } from '../ui/Label';
-import { CheckedState } from '@radix-ui/react-checkbox';
-import { useEffect, useState } from 'react';
 
 interface IngredientsListProps {
   ingredients: SafeIngredient[];
@@ -13,32 +15,54 @@ interface IngredientsListProps {
 const IngredientsList: React.FC<IngredientsListProps> = ({ ingredients }) => {
   const [ingredientsList, setIngredientsList] = useState(ingredients);
 
-  const crossOffIngredient = (checkedState: CheckedState, index: number) => {
-    setIngredientsList((prevData) => {
-      return prevData.map((ingredient, i) =>
-        i === index ? { ...ingredient, isChecked: !!checkedState } : ingredient
-      );
-    });
-  };
+  let [ref, animate] = useAnimate();
 
-  useEffect(() => {
-    setIngredientsList(ingredients);
-  }, [ingredients]);
+  const handleChange = (checked: boolean, index: number) => {
+    let updatedIngredients = ingredientsList.map((ingredient, idx) =>
+      index === idx ? { ...ingredient, isChecked: checked } : ingredient
+    );
+
+    setIngredientsList(updatedIngredients);
+
+    let lastCompletedItem = updatedIngredients.findIndex((i) => !i.isChecked);
+
+    if (checked) {
+      if (updatedIngredients.every((ingredient) => ingredient.isChecked)) {
+        animate(
+          'input',
+          { scale: [1, 1.25, 1] },
+          {
+            duration: 0.35,
+            delay: stagger(0.075, { from: lastCompletedItem }),
+          }
+        );
+      } else {
+        animate(
+          'input',
+          { rotate: [0, 10, -10, 0] },
+          {
+            duration: 0.5,
+            delay: stagger(0.1, { from: lastCompletedItem }),
+          }
+        );
+      }
+    }
+  };
 
   return (
     <div className="flex flex-col items-center sm:items-start basis-1/3">
       <h4 className="text-xl font-semibold tracking-tight mb-2">Ingredients</h4>
-      <ul className="list-none">
+      <ul ref={ref} className="list-none">
         {ingredientsList.map((ingredient, index) => (
           <li
             key={index}
             className="flex items-center space-x-2 mt-2 leading-7"
           >
-            <Checkbox
-              onCheckedChange={(checkedState: CheckedState) =>
-                crossOffIngredient(checkedState, index)
-              }
-              className="rounded-full text-amber-500 data-[state=checked]:bg-amber-500 data-[state=checked]:border-amber-500 data-[state=checked]:text-white"
+            <input
+              onChange={(e) => handleChange(e.target.checked, index)}
+              checked={ingredient.isChecked}
+              type="checkbox"
+              className="h-5 w-5 rounded-md border-2 border-gray-300 text-amber-500 transition-colors duration-300 focus:ring-0 focus:ring-offset-0 focus-visible:ring-2 focus-visible:ring-amber-500/50 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-100 group-active:border-amber-500 group-active:checked:text-amber-500/25"
             />
             <Label
               className={
