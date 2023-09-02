6099ad291ef1dc168c0ab685000344a0a81a402a
jormarcus <jormarcus92@gmail.com>
2023-09-02 07:02:45 -0400

    Ingredients animation change

--- src/components/recipes/IngredientsList.tsx
+++ src/components/recipes/IngredientsList.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import { useState } from 'react';
-import { CheckedState } from '@radix-ui/react-checkbox';
-import { motion, stagger, useAnimate } from 'framer-motion';
+import { ChangeEvent, useState } from 'react';
+import { stagger, useAnimate } from 'framer-motion';
 
 import { SafeIngredient } from '@/types';
-import { Checkbox } from '../ui/Checkbox';
 import { Label } from '../ui/Label';
 
 interface IngredientsListProps {
@@ -17,7 +15,9 @@ const IngredientsList: React.FC<IngredientsListProps> = ({ ingredients }) => {
 
   let [ref, animate] = useAnimate();
 
-  const handleChange = (checked: boolean, index: number) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+    const checked: boolean = e.target.checked;
+
     let updatedIngredients = ingredientsList.map((ingredient, idx) =>
       index === idx ? { ...ingredient, isChecked: checked } : ingredient
     );
@@ -26,26 +26,26 @@ const IngredientsList: React.FC<IngredientsListProps> = ({ ingredients }) => {
 
     let lastCompletedItem = updatedIngredients.findIndex((i) => !i.isChecked);
 
+    if (
+      checked &&
+      updatedIngredients.every((ingredient) => ingredient.isChecked)
+    ) {
+      animate(
+        'input',
+        { scale: [1, 1.25, 1] },
+        { duration: 0.35, delay: stagger(0.075, { from: lastCompletedItem }) }
+      );
+    }
+
     if (checked) {
-      if (updatedIngredients.every((ingredient) => ingredient.isChecked)) {
-        animate(
-          'input',
-          { scale: [1, 1.25, 1] },
-          {
-            duration: 0.35,
-            delay: stagger(0.075, { from: lastCompletedItem }),
-          }
-        );
-      } else {
-        animate(
-          'input',
-          { rotate: [0, 10, -10, 0] },
-          {
-            duration: 0.5,
-            delay: stagger(0.1, { from: lastCompletedItem }),
-          }
-        );
-      }
+      animate(
+        e.target,
+        { rotate: [0, 10, -10, 0] },
+        {
+          duration: 0.5,
+          delay: stagger(0.1, { from: lastCompletedItem }),
+        }
+      );
     }
   };
 
@@ -59,16 +59,15 @@ const IngredientsList: React.FC<IngredientsListProps> = ({ ingredients }) => {
             className="flex items-center space-x-2 mt-2 leading-7"
           >
             <input
-              onChange={(e) => handleChange(e.target.checked, index)}
-              checked={ingredient.isChecked}
+              onChange={(e) => handleChange(e, index)}
               type="checkbox"
               className="h-5 w-5 rounded-md border-2 border-gray-300 text-amber-500 transition-colors duration-300 focus:ring-0 focus:ring-offset-0 focus-visible:ring-2 focus-visible:ring-amber-500/50 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-100 group-active:border-amber-500 group-active:checked:text-amber-500/25"
             />
             <Label
               className={
                 ingredient.isChecked
-                  ? 'line-through  whitespace-nowrap'
-                  : ' whitespace-nowrap'
+                  ? 'line-through whitespace-nowrap'
+                  : 'whitespace-nowrap'
               }
             >
               {ingredient.input}
