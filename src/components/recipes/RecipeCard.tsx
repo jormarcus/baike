5319ba421aaba1e35af2c05f4cbcaf4a5ecd2d39
jormarcus <jormarcus92@gmail.com>
2023-09-03 15:19:59 -0400

    Recipe card redesign and animation

--- src/components/recipes/RecipeCard.tsx
+++ src/components/recipes/RecipeCard.tsx
@@ -1,13 +1,12 @@
 'use client';
 
 import Image from 'next/image';
-import { FaRegHeart, FaStar } from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
 import { Croissant, Heart } from 'lucide-react';
 import Link from 'next/link';
-import { useMemo } from 'react';
 
 import { SafeRecipe } from '@/types';
-import useWindowWidth from '@/hooks/useWindowWidth';
+import FeatureCard from './FeatureCard';
 
 interface RecipeCardProps {
   recipe: SafeRecipe;
@@ -16,21 +15,22 @@ interface RecipeCardProps {
 const CardContent: React.FC<{
   image: string | null;
   name: string;
-  size: number;
-}> = ({ image, name, size }) => (
-  <div className="relative flex items-center justify-center aspect-square w-full overflow-hidden rounded-xl">
-    {image ? (
-      <Image
-        fill
-        sizes="(max-width: 768px) 33vw, (max-width: 1200px) 50vw, 33vw"
-        src={image}
-        alt={name}
-        decoding="async"
-        className="h-full w-full object-cover transition group-hover:scale-110"
-      />
-    ) : (
-      <Croissant size={size} />
-    )}
+}> = ({ image, name }) => (
+  <div className="relative aspect-square overflow-hidden rounded-3xl">
+    <div className="h-full w-full object-cover transition duration-300 group-hover:scale-110 ease-cubic-bezier rounded-3xl flex items-center justify-center">
+      {image ? (
+        <Image
+          fill
+          sizes="(max-width: 768px) 33vw, (max-width: 1200px) 50vw, 33vw"
+          src={image}
+          alt={name}
+          decoding="async"
+          className="object-cover aspect-square"
+        />
+      ) : (
+        <Croissant size={60} />
+      )}
+    </div>
     <div className="absolute right-3 top-3">
       <Heart
         size={20}
@@ -44,7 +44,7 @@ const CardFooter: React.FC<{
   name: string;
   averageRating: number;
 }> = ({ name, averageRating }) => (
-  <div className="flex flex-col pt-2">
+  <div className="flex flex-col pt-2 pb-4 px-4">
     <div className="flex flex-row justify-between items-center gap-3">
       <div className="text-md font-semibold truncate">{name}</div>
       <div className="flex flex-row gap-2 items-center">
@@ -57,26 +57,14 @@ const CardFooter: React.FC<{
 
 const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => {
   const { id, name, imageSrc } = recipe;
-  // next js has an error on refresh window is not defined
-  const width = useWindowWidth();
-
-  const iconSize = useMemo(() => {
-    let size = 80;
-    if (width < 768) {
-      size = 70;
-    } else if (width < 1024) {
-      size = 80;
-    }
-    return size;
-  }, [width]);
 
   return (
-    <div className="flex flex-col gap-2 group col-span-1 p-3 group w-full overflow-hidden rounded-3xl border bg-gradient-to-b from-neutral-50/90 to-neutral-100/90 transition duration-300 dark:from-neutral-950/90 dark:to-neutral-800/90">
+    <FeatureCard>
       <Link href={`/recipe/${id}`}>
-        <CardContent image={imageSrc} name={name} size={iconSize} />
+        <CardContent image={imageSrc} name={name} />
         <CardFooter name={name} averageRating={5} />
       </Link>
-    </div>
+    </FeatureCard>
   );
 };
 
--- src/components/recipes/RecipesList.tsx
+++ src/components/recipes/RecipesList.tsx
@@ -10,7 +10,7 @@ const RecipesList: React.FC<RecipesListProps> = ({ recipes }) => {
   return (
     <div>
       {recipes && recipes.length > 0 ? (
-        <div className="grid grid-cols-1 gap-6 pt-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 grid-auto-rows-auto">
+        <div className="grid grid-cols-1 gap-6 py-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 grid-auto-rows-auto">
           {recipes.map((recipe) => (
             <RecipeCard key={recipe.id} recipe={recipe} />
           ))}
