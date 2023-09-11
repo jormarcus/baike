ddf24c15304de390f0a8a6806b3fdfd3dce5c2ee
jormarcus <jormarcus92@gmail.com>
2023-09-10 20:25:54 -0400

    Logo change

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -6,15 +6,24 @@ import { cn } from '@/lib/utils';
 import { HTMLAttributes } from 'react';
 import Image from 'next/image';
 
-interface LogoProps extends HTMLAttributes<HTMLDivElement> {}
+interface LogoProps extends HTMLAttributes<HTMLDivElement> {
+  isCollapsed: boolean;
+}
 
-const Logo: React.FC<LogoProps> = ({ className }) => {
+const Logo: React.FC<LogoProps> = ({ isCollapsed, className }) => {
   return (
     <Link href="/" className="flex items-center justify-center gap-2">
       <Image src="/images/bake.png" width={40} height={40} alt="Baike logo" />
-      <h2 className={cn('text-2xl font-bold cursor-pointer', className)}>
-        Baike
-      </h2>
+      {!isCollapsed && (
+        <h2
+          className={cn(
+            'text-2xl font-bold hover:text-amber-500 transition duration-500',
+            className
+          )}
+        >
+          Baike
+        </h2>
+      )}
     </Link>
   );
 };
