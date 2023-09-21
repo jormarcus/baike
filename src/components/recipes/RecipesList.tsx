72511fd8726d1b96820e6413a6813e195d3811b4
jormarcus <jormarcus92@gmail.com>
2023-09-21 13:53:48 -0400

    Recipes search fix

--- src/components/recipes/RecipesList.tsx
+++ src/components/recipes/RecipesList.tsx
@@ -20,25 +20,33 @@ const RecipesList: React.FC<RecipesListProps> = ({
   totalCount,
 }) => {
   const searchParams = useSearchParams();
-  const [recipes, setRecipes] = useState<SafeRecipe[]>(initialRecipes);
+
+  const [recipes, setRecipes] = useState<SafeRecipe[]>([]);
 
   const container = useRef<HTMLDivElement>(null);
   const options = {};
   const isVisible = useIntersectionObserver(container, options);
 
   useEffect(() => {
+    setRecipes(initialRecipes);
+  }, [initialRecipes]);
+
+  useEffect(() => {
     const getRecipes = async () => {
       try {
         const skip = recipes.length;
-        if (skip >= totalCount) return;
+        if (skip >= totalCount) {
+          return;
+        }
+
+        const query: string = searchParams.get('search') || '';
 
         const fetchedRecipes: SafeRecipe[] = await getPaginatedRecipes(
-          searchParams.get('search') || '',
+          query,
           '',
           skip
         );
         setRecipes((prevRecipes) => [...prevRecipes, ...fetchedRecipes]);
-        console.log('fetchedRecipes', fetchedRecipes);
       } catch (error) {
         console.error(error);
       }
@@ -52,15 +60,16 @@ const RecipesList: React.FC<RecipesListProps> = ({
   return (
     <div>
       <div className="grid grid-cols-1 gap-6 py-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 grid-auto-rows-auto">
-        {recipes.map((recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} />
-        ))}
+        {recipes.map((recipe, index) => {
+          return index === recipes.length - 1 && index < totalCount ? (
+            <div ref={container} key={recipe.id} className="w-full">
+              <RecipeCard recipe={recipe} />
+            </div>
+          ) : (
+            <RecipeCard key={recipe.id} recipe={recipe} />
+          );
+        })}
       </div>
-      {totalCount > recipes.length && (
-        <div ref={container} className="w-full">
-          <Loading />
-        </div>
-      )}
     </div>
   );
 };
--- src/components/recipes/RecipesPageHeader.tsx
+++ src/components/recipes/RecipesPageHeader.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Link from 'next/link';
 import { Croissant, PlusCircle } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 import ImportRecipeModal from '../modals/ImportRecipeModal';
 import { Button } from '../ui/Button';
@@ -10,12 +10,14 @@ import Searchbox from '../ui/Searchbox';
 interface RecipesPageHeaderProps {}
 
 const RecipesPageHeader: React.FC<RecipesPageHeaderProps> = () => {
+  const router = useRouter();
+
   return (
     <div className="sticky top-0 z-[15] border border-border/60 dark:border-border/80 divide-border/60 dark:divide-border/80 ring-border dark:ring-border bg-background dark:bg-background">
       <div className="py-4 md:py-8 mx-auto max-w-screen-xl px-4 md:px-12 flex flex-row justify-between items-center gap-4">
         <div className="flex items-center gap-4">
-          <Croissant size={40} />
-          <h2 className="text-4xl">Recipes</h2>
+          <Croissant size={30} />
+          <h2 className="text-3xl">Recipes</h2>
         </div>
 
         <div className="flex items-center justify-center max-w-md w-full">
@@ -24,16 +26,15 @@ const RecipesPageHeader: React.FC<RecipesPageHeaderProps> = () => {
 
         <div className="flex flex-row gap-3">
           <ImportRecipeModal />
-          <Link href="/recipes/add">
-            <Button
-              className="bg-amber-500 text-white hover:bg-amber-400 flex gap-2"
-              variant="default"
-              onClick={() => console.log('Add recipe')}
-            >
-              <PlusCircle />
-              Add recipe
-            </Button>
-          </Link>
+
+          <Button
+            className="bg-amber-500 text-white hover:bg-amber-400 flex gap-2"
+            variant="default"
+            onClick={() => router.push('/recipes/add')}
+          >
+            <PlusCircle />
+            Add recipe
+          </Button>
         </div>
       </div>
     </div>
