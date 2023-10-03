844f224b62381ec47a1e264107f003ceb2a8ca71
jormarcus <jormarcus92@gmail.com>
2023-10-03 11:50:54 -0400

    Fix allergy and avoidance routes

--- src/app/api/allergies/route.ts
+++ src/app/api/allergies/route.ts
@@ -1,28 +1,23 @@
+import { NextResponse } from 'next/server';
+
 import {
   createAllergies,
   getAllergies,
 } from '@/app/_actions/user-preference-actions';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { NextResponse } from 'next/server';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    const allergies = await getAllergies();
-    console.log('allergies in route', allergies);
-    return NextResponse.json({ data: allergies });
-  }
+export async function GET(req: Request, res: Response) {
+  const allergies = await getAllergies();
+  console.log('allergies in route', allergies);
+  return NextResponse.json({ data: allergies });
 }
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const allergies = await createAllergies(req.body);
-    return NextResponse.json({ data: allergies });
-  }
+export async function POST(req: Request, res: Response) {
+  const body = await req.json();
+  const allergies = await createAllergies(body);
+  return NextResponse.json({ data: allergies });
 }
 
-export async function PUT(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'PUT') {
-    // const allergies = await createAllergies(req.body);
-    // return NextResponse.json({ data: allergies });
-  }
+export async function PUT(req: Request, res: Response) {
+  // const allergies = await createAllergies(req.body);
+  // return NextResponse.json({ data: allergies });
 }
--- src/app/api/avoidances/route.ts
+++ src/app/api/avoidances/route.ts
@@ -1,28 +1,24 @@
+import { NextResponse } from 'next/server';
+
 import {
   createAvoidances,
   getAvoidances,
 } from '@/app/_actions/user-preference-actions';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { NextResponse } from 'next/server';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    const avoidances = await getAvoidances();
-    console.log('avoidances in route', avoidances);
-    return NextResponse.json({ data: avoidances });
-  }
+export async function GET(req: Request, res: Response) {
+  const avoidances = await getAvoidances();
+  console.log('avoidances in route', avoidances);
+  return NextResponse.json({ data: avoidances });
 }
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const avoidances = await createAvoidances(req.body);
-    return NextResponse.json({ data: avoidances });
-  }
+export async function POST(req: Request, res: Response) {
+  const body = await req.json();
+  const avoidances = await createAvoidances(body);
+  return NextResponse.json({ data: avoidances });
 }
 
-export async function PUT(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'PUT') {
-    // const avoidances = await createAvoidances(req.body);
-    // return NextResponse.json({ data: avoidances });
-  }
+export async function PUT(req: Request, res: Response) {
+  const body = await req.json();
+  // const avoidances = await createAvoidances(body);
+  // return NextResponse.json({ data: avoidances });
 }
