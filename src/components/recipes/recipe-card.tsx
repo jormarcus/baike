a74e316a864a3e9ed8139e83f9aa8b0c63f93df2
jormarcus <jormarcus92@gmail.com>
2024-04-28 15:11:41 -0400

    feat(unauth-page): fix spacing, change card footer color and refactor

--- src/components/recipes/recipe-card.tsx
+++ src/components/recipes/recipe-card.tsx
@@ -10,13 +10,43 @@ import { Label } from '../ui/label';
 import { useRecipeCompare } from '@/context/recipe-compare-context';
 import { Card, CardContent, CardFooter } from '@/components/ui/card';
 
-interface RecipeCardProps {
+const Footer: React.FC<{ recipe: SafeRecipe }> = ({ recipe }) => {
+  const { handleRecipeSelect } = useRecipeCompare();
+  const { averageRating, id, name } = recipe;
+
+  return (
+    <CardFooter className="flex flex-col items-start pt-1 pb-0 px-0">
+      <div className="flex justify-between items-center w-full">
+        <Link
+          href={`/recipes/${id}`}
+          className="text-sm font-semibold truncate basis-10/12"
+        >
+          {name}
+        </Link>
+
+        <div className="flex flex-row gap-[2px] items-center justify-end basis-2/12">
+          <FaStar size={14} />
+          <div className="text-md font-semibold">{averageRating || 0}</div>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2 mt-2 leading-7 text-muted-foreground">
+        <input
+          onChange={() => handleRecipeSelect(recipe)}
+          type="checkbox"
+          className="h-5 w-5 rounded-lg border-2 border-gray-300 text-amber-500 transition-colors duration-300 focus:ring-0 focus:ring-offset-0 focus-visible:ring-2 focus-visible:ring-amber-500/50 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-100 group-active:border-amber-500 group-active:checked:text-amber-500/2 cursor-pointer"
+        />
+        <Label>Compare</Label>
+      </div>
+    </CardFooter>
+  );
+};
+
+type RecipeCardProps = {
   recipe: SafeRecipe;
-}
+};
 
 const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => {
   const { id, name, imageSrc } = recipe;
-  const { handleRecipeSelect } = useRecipeCompare();
 
   return (
     <Card className="border-none rounded-lg">
@@ -33,31 +63,7 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => {
           </div>
         </CardContent>
       </Link>
-      <CardFooter className="flex flex-col items-start pt-1 pb-0 px-0">
-        <div className="flex justify-between items-center w-full">
-          <Link
-            href={`/recipes/${id}`}
-            className="text-sm font-semibold truncate basis-10/12"
-          >
-            {recipe.name}
-          </Link>
-
-          <div className="flex flex-row gap-[2px] items-center justify-end basis-2/12">
-            <FaStar size={14} />
-            <div className="text-md font-semibold">
-              {recipe.averageRating || 0}
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center space-x-2 mt-2 leading-7">
-          <input
-            onChange={() => handleRecipeSelect(recipe)}
-            type="checkbox"
-            className="h-5 w-5 rounded-lg border-2 border-gray-300 text-amber-500 transition-colors duration-300 focus:ring-0 focus:ring-offset-0 focus-visible:ring-2 focus-visible:ring-amber-500/50 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-100 group-active:border-amber-500 group-active:checked:text-amber-500/2 cursor-pointer"
-          />
-          <Label>Compare</Label>
-        </div>
-      </CardFooter>
+      <Footer recipe={recipe} />
     </Card>
   );
 };
